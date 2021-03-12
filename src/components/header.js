79877d55abb373a25fd621ba994f8522ee12ab44
Miles Crighton <miles.crighton@gmail.com>
2021-03-12 10:59:10 +0000

    Moved timer to Header component

--- src/components/header.js
+++ src/components/header.js
@@ -2,34 +2,71 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+  const [counter, setCounter] = React.useState(0)
+  const [running, setRunning] = React.useState(true)
+
+  React.useEffect(() => {
+    let interval
+
+    if (running) {
+      interval = setInterval(() => setCounter(x => x + 1), 100)
+    }
+    return () => {
+      if (interval) clearInterval(interval)
+    }
+  }, [running])
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+      <div style={{}}>
+        <div
           style={{
-            color: `white`,
-            textDecoration: `none`,
+            display: "flex",
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `1.45rem 1.0875rem`,
           }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+          <h1 style={{ margin: 0, marginRight: "1rem" }}>
+            <Link
+              to="/"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h1>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <button
+              onClick={() => setRunning(x => !x)}
+              style={{ marginRight: "1rem" }}
+            >
+              {running ? "Pause" : "Play"}
+            </button>
+            <div
+              style={{
+                display: "inline-block",
+
+                color: "white",
+                fontSize: 30,
+                fontWeight: "bold",
+              }}
+            >
+              {counter / 10}
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
