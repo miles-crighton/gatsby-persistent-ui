b2e3fedec4c60ed5ca97833eff03648b93514abc
Miles Crighton <miles.crighton@gmail.com>
2021-03-12 11:01:13 +0000

    Update project metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Persistent UI With Gatsby`,
+    description: `Learn how to utilize wrapPageElement to keep components persist across routes`,
+    author: `@miles-crighton`,
   },
-  plugins: [],
 }
